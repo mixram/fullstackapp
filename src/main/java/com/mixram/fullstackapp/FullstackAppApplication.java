137d5e670fd43e84a3c4156404d528de7b68e58e
mixram <mixram@gmail.com>
2021-05-13 12:34:31 +0300

    * initial commit

---
+++ src/main/java/com/mixram/fullstackapp/FullstackAppApplication.java
@@ -0,0 +1,13 @@
+package com.mixram.fullstackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackAppApplication.class, args);
+    }
+
+}
